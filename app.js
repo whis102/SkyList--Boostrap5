4897b1833978f2505869d5f47c260ebe1409f8ef
whis102 <chicongnguyen.dev@gmail.com>
2024-05-31 20:05:33 +0700

    push thread chat

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 function updateTime() {
-    const now = new Date()
-    const date = document.getElementById('current-date')
-    const options = {
-        weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'
-    }
-    const format = now.toLocaleDateString('en-US', options)
+  const now = new Date();
+  const date = document.getElementById("current-date");
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const format = now.toLocaleDateString("en-US", options);
 
-    date.innerText = `Today, ${format}`
+  date.innerText = `Today, ${format}`;
 }
 
-updateTime()
+updateTime();
 
-setInterval(updateTime, 1000)
+setInterval(updateTime, 1000);
+
+// add Task
+const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById("input-box");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You should write down something");
+  } else {
+    let li = document.createElement("li");
+    li.classList.add("list-group-item");
+
+    let checkbox = document.createElement("input");
+    checkbox.classList.add("form-check-input", "me-1");
+    checkbox.type = "checkbox";
+    checkbox.value = "";
+    checkbox.setAttribute("aria-label", "...");
+
+    let text = document.createTextNode(inputBox.value);
+    li.appendChild(checkbox);
+    li.appendChild(text);
+
+    listContainer.appendChild(li);
+  }
+}
